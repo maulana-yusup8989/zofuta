adebf48fdff0d15874f4d1db40c983bffb7d88d2
Maulana Yusup <maulana.yusup89899@gmail.com>
2022-11-19 05:40:22 +0700

    front-end su-admin

---
+++ App/home.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Zofuta | Selamat Datang</title>
+    <!-- Custom fonts for this template-->
+    <link href="../node_modules/bootstrap/css/all.css" rel=stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="../node_modules/bootstrap/css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/mycss.css">
+</head>
+
+<body background="img/Lapangan/lapangan.png" style="background-repeat:no-repeat; background-size: 100%;">
+
+    <!-- navbar -->
+    <nav class="navbar navbar-dark navbar-expand-lg">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a class="navbar-brand" href="#">Zona Futsal Purwakarta</a>
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="login.php" class="btn btn-success">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- end-navbar -->
+
+    <!-- body -->
+    <div class="title text-light">
+        <p>Selamat datang di Zona Futsal Purwakarta</p>
+        <a href="menu.php" class="btn btn-success">Boking Sekarang !!!</a>
+    </div>
+    <!-- end-body -->
+
+    <!-- Bootstrap core JavaScript-->
+    <script src=" ../node_modules/bootstrap/js/jquery.js">
+    </script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="../node_modules/bootstrap/js/jquery.easing.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="../node_modules/bootstrap/js/sb-admin-2.min.js"></script>
+</body>
+
+</html>
---
+++ App/register.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Zofuta | Register</title>
+    <!-- Custom fonts for this template-->
+    <link href="../node_modules/bootstrap/css/all.min.css" rel=stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="../node_modules/bootstrap/css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gradient-primary">
+    <div class="container">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+            <div class="col-lg-7">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Silakan Daftar Akun</h1>
+                                    </div>
+                                    <form class="user">
+                                        <div class="form-group">
+                                            <input type="username" class="form-control form-control-user" id="username" placeholder="Username">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="name" class="form-control form-control-user" id="name" placeholder="Name Lengkap">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user" id="email" aria-describedby="emailHelp" placeholder="Email">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="no_hp" class="form-control form-control-user" id="no_hp" placeholder="+62">
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                                <input type="password" class="form-control form-control-user" id="password" placeholder="katasandi">
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <input type="repeat_password" class="form-control form-control-user" id="repeat_password" placeholder="Konfirmasi katasandi">
+                                            </div>
+                                        </div>
+                                        <a href=.#" class="btn btn-success btn-user btn-block">
+                                            Login
+                                        </a>
+                                    </form>
+                                    <hr>
+                                </div>
+                                <div class="text-center">
+                                    <a class="small" href="login.php">Login!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="../node_modules/bootstrap/js/jquery.js"></script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="../node_modules/bootstrap/js/jquery.easing.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="../node_modules/bootstrap/js/sb-admin-2.min.js"></script>
+</body>
+
+</html>

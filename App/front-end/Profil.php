c5ad1752d1a890b262652beb5779353c21d75995
Maulana Yusup <maulana.yusup89899@gmail.com>
2022-11-25 23:11:55 +0700

    Profil

--- App/front-end/Profil.php
+++ App/front-end/Profil.php
@@ -21,68 +21,171 @@
     <link href="../../template/Gp/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="../../template/Gp/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="../../template/Gp/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-
     <!-- Template Main CSS File -->
     <link href="../../template/Gp/assets/css/styles.css" rel="stylesheet">
 </head>
 
 <body>
-    <!-- ======= Header ======= -->
-    <header id="header" class="fixed-top ">
-        <div class="container d-flex align-items-center justify-content-lg-between">
+    <main id="main">
+        <!-- ======= Keterangan Gor ======= -->
+        <section id="features" class="features">
+            <div class="container" data-aos="fade-up" style="margin-top: 100px;">
+                <div class="row bordered " style="background-color:#EFEFEF">
+                    <div class=" col-lg-3 d-flex justify-content-center" data-aos="fade-left" data-aos-delay="100">
+                        <div class="icon-box mt-5 " data-aos="zoom-in" data-aos-delay="150">
+                            <img src="../img/logo.png" style="width: 150px; height: 200px;" alt="">
+                        </div>
+                        <div class="icon-box mt-5" data-aos="zoom-in" data-aos-delay="150">
+                        </div>
+                    </div>
+                    <div class="col-lg-9 pt-3 pl-3 " data-aos=" fade-right">
+                        <h2 class="fw-bold ">Bryan jonathan jacobo</h2>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button class="btn btn-success fw-bold" type="button">Edit</button>
+                        </div>
+                        <div class="row">
+                            <div class="col pl-3 mt-3">
+                                <h4 class="fw-bold">Biodata</h4>
+                            </div>
+                            <div class="col pl-3 mb-2 mt-3">
+                                <h4 class="fw-bold">Akun</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col pl-3">
+                                <h5 class="fw-bold">Nama Depan</h4>
+                            </div>
+                            <div class="col pl-3">
+                                <h5 class="fw-bold">Username</h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col pl-3">
+                                <h5>Bryan</h5>
+                            </div>
+                            <div class="col pl-3">
+                                <h5>Bryan99</h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col pl-3">
+                                <h5 class="fw-bold">Nama Belakang</h4>
+                            </div>
+                            <div class="col pl-3">
+                                <h5 class="fw-bold">Password</h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col pl-3">
+                                <h5>jonathan jacobo</h5>
+                            </div>
+                            <div class="col pl-3">
+                                <h5>*******************</h5>
+                            </div>
+                        </div>
+                        <h2 class="fw-bold mt-4 ">Status Pemesanan Anda</h2>
+                        <div class="row">
+                            <div class="col">
+                                <table class="table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Tanggal Main</th>
+                                            <th scope="col">Gor</th>
+                                            <th scope="col">Lapang</th>
+                                            <th scope="col">Jam</th>
+                                            <th scope="col">Verifikasi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>19 Nov 2022</td>
+                                            <td>King futsal</td>
+                                            <td>Lapangan 1</td>
+                                            <td>06.00 - 08.00</td>
+                                            <td>
+                                                <a class="text-light bg-warning fw-bold">Menunggu Konfirmasi</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-9 pt-3 pl-3 " style="background-color: #EFEFEF;" data-aos=" fade-right">
+                    </div>
+                </div>
+                <h2 class="fw-bold mt-4 ">Riwayat Pemesanan</h2>
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Tanggal Main</th>
+                            <th scope="col">Gor</th>
+                            <th scope="col">Lapang</th>
+                            <th scope="col">Jam</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>19 Nov 2022</td>
+                            <td>King futsal</td>
+                            <td>Lapangan 1</td>
+                            <td>06.00 - 08.00</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>17 November 2022</td>
+                            <td>Blessing futsal</td>
+                            <td>Lapangan 2</td>
+                            <td>06.00 - 08.00</td>
+                        </tr>
+                    </tbody>
+                </table>
 
-            <h1 class="logo auto me-lg-0"><a href="index.php" class="logo auto lg-0"><img src="../img/logo.png" alt="" class="img-fluid px-4"></a><a href="index.php"><span>Zona Futsal</span> Purwakarta.</a></h1>
-            <!-- Uncomment below if you prefer to use an image logo -->
-            <nav id="navbar" class="navbar order-lg-0">
-                <ul>
-                    <li><a class="nav-link scrollto active" onclick="scrollwin()">Home</a></li>
-                    <li><a class="nav-link scrollto" href="#">Contact</a></li>
-                    <button class="btn btn-success scrollto fw-bold" onclick="window.location.href='login.php';" style=" margin-left: 20px;">Login</button>
-                </ul>
-
-                <i class=" bi bi-list mobile-nav-toggle"></i>
-            </nav><!-- .navbar -->
-        </div>
-    </header><!-- End Header -->
-
-    <!-- ======= Hero Section ======= -->
-
-    <div class="container">
-        <div class="row">
-            <div class="col">
-.
             </div>
-        </div>
-    </div>
-
-
-    <!-- ======= Footer ======= -->
-    <footer id="footer mt-4">
+        </section><!-- End-Keterangan Gor -->
+        <!-- ======= Header ======= -->
+        <header id="header" class="fixed-top ">
+            <div class="container d-flex align-items-center justify-content-lg-between">
+                <h1 class="logo auto me-lg-0"><a href="index.php" class="logo auto lg-0"><img src="../img/logo.png" alt="" class="img-fluid px-4"></a><a href="index.php"><span>Zona Futsal</span> Purwakarta.</a></h1>
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <nav id="navbar" class="navbar order-lg-0">
+                    <ul>
+                        <li><a class="nav-link scrollto active" onclick="scrollwin()">Home</a></li>
+                        <li><a class="nav-link scrollto" href="#">Contact</a></li>
+                        <button class="btn btn-success scrollto fw-bold" onclick="window.location.href='login.php';" style=" margin-left: 20px;">Login</button>
+                    </ul>
+                    <i class=" bi bi-list mobile-nav-toggle"></i>
+                </nav><!-- .navbar -->
+            </div>
+        </header><!-- End Header -->
+        <!-- ======= Hero Section ======= -->
         <div class="container">
-            <div class="copyright">
-                <strong><span style="color:green;">&copy; ZONA FUTSAL</span> PURWAKARTA</strong>
+            <div class="row">
+                <div class="col">
+                </div>
             </div>
         </div>
-    </footer><!-- End Footer -->
+        <!-- ======= Footer ======= -->
+        <footer id="footer mt-4">
+            <div class="container">
+                <div class="copyright">
+                    <strong><span style="color:green;">&copy; ZONA FUTSAL</span> PURWAKARTA</strong>
+                </div>
+            </div>
+        </footer><!-- End Footer -->
+        <!-- Vendor JS Files -->
+        <script src="../../template/Gp/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="../../template/Gp/assets/vendor/aos/aos.js"></script>
+        <script src="../../template/Gp/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="../../template/Gp/assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="../../template/Gp/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="../../template/Gp/assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="../../template/Gp/assets/vendor/php-email-form/validate.js"></script>
 
-    <!-- Vendor JS Files -->
-    <script src="../../template/Gp/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-    <script src="../../template/Gp/assets/vendor/aos/aos.js"></script>
-    <script src="../../template/Gp/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../../template/Gp/assets/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="../../template/Gp/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="../../template/Gp/assets/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="../../template/Gp/assets/vendor/php-email-form/validate.js"></script>
-
-    <!-- Template Main JS File -->
-    <script src="../../template/Gp/assets/js/main.js"></script>
-    <script>
-        function scrollwin() {
-            window.scrollTo(0, 900);
-        }
-    </script>
-
+        <!-- Template Main JS File -->
+        <script src="../../template/Gp/assets/js/main.js"></script>
 </body>
 
 </html>

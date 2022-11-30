0408fb1b8b8856d9a60608ed320043faf9968a26
Fahmi A N <dhafin.rr@gmail.com>
2022-11-30 11:31:40 +0700

    lapang.php

--- App/front-end/lapang.php
+++ App/front-end/lapang.php
@@ -119,7 +119,88 @@
                 </h2>
                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    <div class="container">
+                            <div class="row">
+                                <div class="col-3 bg-success ">
+                                    <img src="" alt="lapangan" srcset="lapangan">
+                                </div>
+                                <div class="col-8">
+                                    <div class="row">
+                                        <div class="col ">
+                                            <h4> Harga</h4>
+                                        </div>
+                                        <div class="col">
+                                            <h4> Jenis Lapangan</h4>
+                                        </div>
+                                        <div class="col">
+                                            <h4>Tanggal</h4>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col ">
+                                            <p><?php echo $row['harga_sewa'] ?> /Jam</p>
+                                        </div>
+                                        <div class="col">
+                                            <p><?php echo $row['ket_lapangan'] ?></p>
+                                        </div>
+                                        <div class="col">
+                                            <select class="form-select" id="state" required>
+                                                <option value="">Choose...</option>
+                                                <option>California</option>
+                                            </select>
+                                        </div>
+                                        <div class="container">
+                                            <div class="table-container">
+                                                <table class="table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>06:00 - 07:00 </th>
+                                                            <th>07:00 - 08:00</th>
+                                                            <th>08:00 - 09:00</th>
+                                                            <th>09:00 - 10:00</th>
+                                                            <th>10:00 - 11:00</th>
+                                                            <th>11:00 - 12:00</th>
+                                                            <th>12:00 - 13:00</th>
+                                                            <th>13:00 - 14:00</th>
+                                                            <th>14:00 - 15:00</th>
+                                                            <th>15:00 - 16:00</th>
+                                                            <th>16:00 - 17:00</th>
+                                                            <th>17:00 - 18:00</th>
+                                                            <th>18:00 - 19:00</th>
+                                                            <th>19:00 - 20:00</th>
+                                                            <th>20:00 - 21:00</th>
+                                                            <th>21:00 - 22:00</th>
+                                                            <th>22:00 - 23:00</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                            <td style="color:#cd2e2e ;background-color: #cd2e2e;">BOOKED</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -133,10 +214,89 @@
                 </h2>
                 <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    <div class="container">
+                            <div class="row">
+                                <div class="col-3 bg-success ">
+                                    <img src="" alt="lapangan" srcset="lapangan">
+                                </div>
+                                <div class="col-8">
+                                    <div class="row">
+                                        <div class="col ">
+                                            <h4> Harga</h4>
+                                        </div>
+                                        <div class="col">
+                                            <h4> Jenis Lapangan</h4>
+                                        </div>
+                                        <div class="col">
+                                            <h4>Tanggal</h4>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col ">
+                                            <p><?php echo $row['harga_sewa'] ?> /Jam</p>
+                                        </div>
+                                        <div class="col">
+                                            <p><?php echo $row['ket_lapangan'] ?></p>
+                                        </div>
+                                        <div class="col">
+                                            <select class="form-select" id="state" required>
+                                                <option value="">Choose...</option>
+                                                <option>California</option>
+                                            </select>
+                                        </div>
+                                        <div class="container">
+                                            <div class="table-container">
+                                                <table class="table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>06:00 - 07:00 </th>
+                                                            <th>07:00 - 08:00</th>
+                                                            <th>08:00 - 09:00</th>
+                                                            <th>09:00 - 10:00</th>
+                                                            <th>10:00 - 11:00</th>
+                                                            <th>11:00 - 12:00</th>
+                                                            <th>12:00 - 13:00</th>
+                                                            <th>13:00 - 14:00</th>
+                                                            <th>14:00 - 15:00</th>
+                                                            <th>15:00 - 16:00</th>
+                                                            <th>16:00 - 17:00</th>
+                                                            <th>17:00 - 18:00</th>
+                                                            <th>18:00 - 19:00</th>
+                                                            <th>19:00 - 20:00</th>
+                                                            <th>20:00 - 21:00</th>
+                                                            <th>21:00 - 22:00</th>
+                                                            <th>22:00 - 23:00</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                            <td style="color:#2ECD71 ;background-color: #2ECD71;">AVAILABLE</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
